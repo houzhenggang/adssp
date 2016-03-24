a822e7f2a25701cd3c1af655173ff8b0236ed4b0
willardalong <772413253@qq.com>
2016-03-24 18:25:48 +0800

    s

--- src/naga/main/main_data.c
+++ src/naga/main/main_data.c
@@ -95,10 +95,8 @@ berr naga_data_process_module(hytag_t * hytag)
 	
     DPF_NODE(MOD_NET, hytag, netseg_dp_process);
     //DPF_NODE(MOD_DNET, hytag, dnet_dp_process);
-	//naga_ssp_counter(hytag);
-	
-	naga_ssp_product(hytag);
-
+    //naga_ssp_counter(hytag);
+    //naga_ssp_product(hytag);
     //DPF_NODE(MOD_DMR, hytag, naga_dmr);
 
     //DPF_NODE(MOD_DMR, hytag, naga_domain);
