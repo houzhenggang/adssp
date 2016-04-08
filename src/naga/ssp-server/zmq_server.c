9388c39da0c847fe6f0936f0ee94a09f5d612c16
曾庆龙 <zengqinglong@MacBook-Pro-2.local>
2016-04-08 17:36:31 +0800

    s

--- src/naga/ssp-server/zmq_server.c
+++ src/naga/ssp-server/zmq_server.c
@@ -23,11 +23,21 @@ int zmq_server_init (void)
     void *context = zmq_ctx_new ();
     void *server = zmq_socket (context, ZMQ_REP);
     zmq_bind (server, "tcp://*:5555");
-
+	char buffer[2048];
+	int size;
+	
     int cycles = 0;
     while (1) {
-        char *request = s_recv (server);
 		
+		size=zmq_recv (server, buffer, 2000, 0);
+
+		if(size = -1)
+		{
+			printf("zmq_recv Failed\n");
+			continue;
+		}
+		buffer[size] = 0;
+		printf("recv len(%d) %s\n", size, buffer);
         s_send (server, "OK");
         free (request);
     }
