569cf52ea44332278bca7743fb60e51c080d30ce
曾庆龙 <zengqinglong@MacBook-Pro-2.local>
2016-04-11 19:01:49 +0800

    s

--- src/naga/ssp-server/include/ssp_server.h
+++ src/naga/ssp-server/include/ssp_server.h
@@ -20,6 +20,9 @@ bts_hashtable_t* init_domain_from_file(char *filename);
 ad_struct_t * apply_valid_ad(apply_info_t *, int times);
 
 
+void today_time_str_init();
+
+
 berr usercookes_init();
 
 berr usercookeis_assess_add(char *cookeis, int cookeislen);
