0f8ff38d1f9a1ffb65c1e761c0831be26969e208
曾庆龙 <zengqinglong@MacBook-Pro-2.local>
2016-04-16 18:00:43 +0800

    s

---
+++ src/naga/ssp-server/cmd_init.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "zebra.h"
+#include "version.h"
+#include "getopt.h"
+#include "command.h"
+#include "filter.h"
+#include "prefix.h"
+#include "privs.h"
+#include "vsr_file.h"
+
+void vty_cmd_init()
+{
+	cmdline_vsr_init();
+
+}
+
+void vty_cmd_config_write(struct vty  *vty)
+{
+}
+
